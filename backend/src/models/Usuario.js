694415761af5dc527d59e187ec2e86836bcb8b5b
aaron-77 <aaronhl1313@gmail.com>
2022-04-06 14:46:36 -0500

    creando carpetas de esturtura

---
+++ backend/src/models/Usuario.js
@@ -0,0 +1,37 @@
+const { DataTypes, Model } = require("sequelize");
+const { connection } = require("../db/connection");
+
+//modelo de Bd del usuario
+class Usuario extends Model {}
+Usuario.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    nombredelpropietario: {
+        type: DataTypes.STRING,
+    },
+    username: {
+        type: DataTypes.STRING,
+        validate: {
+            is: ['^([a-zA-zñ_\\-0-9]{8,200})$'],
+            max: 255
+        }
+    },
+    password: {
+        type: DataTypes.STRING
+    },
+    rol: {
+        type: DataTypes.STRING
+    },
+}, {
+    sequelize: connection,
+    modelName: "usuario",
+    createdAt: false,
+    updatedAt: false
+});
+
+
+
+module.exports.Usuario = Usuario;
